72da92e6e244ed3a858a720172577766df2b56fc
Yauhen Hatsukou <egatsukov@gmail.com>
2018-10-23 11:53:36 +0300

    Default captureMode changed to videoWithoutAudio

--- Sources/NextLevel.swift
+++ Sources/NextLevel.swift
@@ -265,7 +265,7 @@ public class NextLevel: NSObject {
     // camera configuration
     
     /// The current capture mode of the device.
-    public var captureMode: NextLevelCaptureMode = .video {
+    public var captureMode: NextLevelCaptureMode = .videoWithoutAudio {
         didSet {
             guard
                 self.captureMode != oldValue
